4631a3dfc6677b25e721cfed5ca11f0465d70a52
Markus Binsteiner <markus@frkl.io>
2021-09-09 23:19:16 +0200

    test: threaded kiara instances

--- tests/test_kiara_context.py
+++ tests/test_kiara_context.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import threading
+import typing
+
 from kiara import Kiara
 
 
@@ -9,3 +12,24 @@ def test_multiple_kiara_instances():
 
     assert kiara.available_operation_ids == kiara_2.available_operation_ids
     assert kiara.available_module_types == kiara_2.available_module_types
+
+
+def test_multiple_kiara_instances_threaded():
+    def create_kiara_context(ops: typing.List[str]):
+        kiara = Kiara()
+        ops.extend(kiara.available_operation_ids)
+
+    ops_1 = []
+    ops_2 = []
+
+    thread_1 = threading.Thread(target=create_kiara_context, args=(ops_1,))
+    thread_1.start()
+
+    thread_2 = threading.Thread(target=create_kiara_context, args=(ops_2,))
+    thread_2.start()
+
+    thread_1.join()
+    thread_2.join()
+
+    assert ops_1
+    assert ops_1 == ops_2

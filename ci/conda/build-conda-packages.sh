f7f8a6da661610fb5160c610881e035f10c938e9
Markus Binsteiner <markus@frkl.io>
2021-09-26 15:26:28 +0200

    ci: conda package build

--- ci/conda/build-conda-packages.sh
+++ ci/conda/build-conda-packages.sh
@@ -13,9 +13,11 @@ if [ -z "$INPUT_DRY_RUN" ]; then
   INPUT_DRY_RUN="true"
 fi
 
+echo $INPUT_DRY_RUN
+
+ANACONDA_USER="dharpa"
 INPUT_OVERRIDE="false"
 INPUT_PLATFORMS="all"
-ANACONDA_PUSH_TOKEN="fr-13dcf55a-6256-4a88-a3ea-60caf84b0453"
 
 CHANNEL_ARGS="-c conda-forge -c dharpa"
 
@@ -33,7 +35,7 @@ check_if_meta_yaml_template_file_exists() {
 }
 
 create_meta_yaml() {
-    if [ ! -z $INPUT_VERSION ]; then
+    if [ -z $INPUT_VERSION ]; then
         echo "No version specified."
         exit 1
     fi
@@ -97,40 +99,40 @@ upload_package(){
     exit 0
     fi
 
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/linux-64/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/linux-64/*.tar.bz2
 
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"osx-64"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/osx-64/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/osx-64/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"osx-arm64"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/osx-arm64/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/osx-arm64/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"linux-32"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/linux-32/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/linux-32/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"linux-ppc64"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/linux-ppc64/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/linux-ppc64/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"linux-ppc64le"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/linux-ppc64le/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/linux-ppc64le/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"linux-s390x"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/linux-s390x/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/linux-s390x/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"linux-armv6l"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/linux-armv6l/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/linux-armv6l/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"linux-armv7l"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/linux-armv7l/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/linux-armv7l/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"linux-aarch64"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/linux-aarch64/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/linux-aarch64/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"win-32"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/win-32/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/win-32/*.tar.bz2
     fi
     if [[ $INPUT_PLATFORMS == *"all"* || $INPUT_PLATFORMS == *"win-64"* ]]; then
-    anaconda upload $ANACONDA_FORCE_UPLOAD --label main build/win-64/*.tar.bz2
+    anaconda upload -u ${ANACONDA_USER} $ANACONDA_FORCE_UPLOAD --label main build/win-64/*.tar.bz2
     fi
 
 }

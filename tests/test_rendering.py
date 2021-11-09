428ea69edb218e0840999c36642fd403e34f9c91
Markus Binsteiner <markus@frkl.io>
2021-11-09 14:23:52 +0100

    test: execute rendered python script for tests

--- tests/test_rendering.py
+++ tests/test_rendering.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+
 import os
 
 from kiara import Kiara
@@ -19,4 +21,24 @@ def test_pipeline_notebook_rendering(kiara: Kiara):
     )
 
     assert rendered
+    # TODO: test notebook content
+
+
+def test_pipeline_script_rendering(kiara: Kiara):
+
+    pipeline = os.path.join(ROOT_DIR, "resources", "pipelines", "logic", "logic_3.json")
+    # pipeline = "/home/markus/projects/dharpa/kiara-playground/examples/streamlit/geolocation_prototype/pipelines/geolocation_1.yml"
+    # template = os.path.join(KIARA_RESOURCES_FOLDER, "templates", "python-script.j2")
+    template = "python-script"
+
+    rendered = kiara.template_mgmt.render(
+        "pipeline", module=pipeline, template=template
+    )
+    assert rendered
+
+    with pytest.raises(Exception) as e:
+        exec(rendered)
+
+    assert "Required pipeline step 'and_1_1'" in str(e.value)
+
     # TODO: test notebook content

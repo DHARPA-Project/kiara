82de519eaac1a8e85198eb4fd07a2ecbe315e4ca
Markus Binsteiner <markus@frkl.io>
2022-02-24 14:45:41 +0100

    docs: work on doc generation for kiara info items

---
+++ src/mkdocstrings_handlers/kiara/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2022, Markus Binsteiner
+#
+#  Mozilla Public License, version 2.0 (see LICENSE or https://www.mozilla.org/en-US/MPL/2.0/)
+
+from kiara.doc.mkdocstrings.handler import get_handler
+
+__all__ = ["get_handler"]  # noqa: WPS410

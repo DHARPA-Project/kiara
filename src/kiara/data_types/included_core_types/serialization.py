54ae62241b33f358f2fe1c1cc94b92d7ca9999c7
Markus Binsteiner <markus@frkl.io>
2022-05-02 20:07:38 +0200

    ci: fix darwin & windows test setup

--- src/kiara/data_types/included_core_types/serialization.py
+++ src/kiara/data_types/included_core_types/serialization.py
@@ -12,63 +12,11 @@ from kiara.data_types.included_core_types.internal import InternalType
 from kiara.defaults import INVALID_HASH_MARKER, INVALID_SIZE_MARKER
 from kiara.models.values.value import SerializedData, Value
 
-# class SerializedValueTypeConfigSchema(DataTypeConfig):
-#
-#     serialization_profile: str = Field(description="The name of the serialization profile.")
-#
-#
-# class SerializedDataType(
-#     InternalType[SerializedValue, SerializedValueTypeConfigSchema]
-# ):
-#     """A data type that contains a serialized representation of a value.
-#
-#     This is used for transferring/streaming value over the wire, and works on a similar principle as the 'load_config'
-#     value type.
-#     """
-#
-#     @classmethod
-#     def python_class(cls) -> Type:
-#         return SerializedValue
-#
-#     @classmethod
-#     def data_type_config_class(cls) -> Type[SerializedValueTypeConfigSchema]:
-#         return SerializedValueTypeConfigSchema
-#
-#     def parse_python_obj(self, data: Any) -> SerializedData:
-#
-#         if isinstance(data, Mapping):
-#             data = SerializedValue(**data)
-#
-#         return data
-#
-#     def _validate(self, value: SerializedValue) -> None:
-#
-#         if not isinstance(value, SerializedValue):
-#             raise ValueError(f"Invalid value type: {type(value)}")
-#
-#     def calculate_hash(self, data: SerializedValue) -> int:
-#         """Calculate the hash of the value."""
-#
-#         return data.serialized_hash
-#
-#     def calculate_size(self, data: SerializedValue) -> int:
-#         return data.size
-#
-#     @property
-#     def serialization_profile(self) -> str:
-#         return self.type_config.serialization_profile
-#
-#     def render_as__terminal_renderable(self, value: Value, render_config: Mapping[str, Any]):
-#
-#         s_val: SerializedValue = value.data
-#         return s_val.create_renderable(**render_config)
-
 
 class PythonObjectType(InternalType[object, DataTypeConfig]):
-    """A data type that contains a serialized representation of a value.
+    """A 'plain' Python object.
 
-    This is used for transferring/streaming value over the wire, and works on a similar principle as the 'load_config'
-    value type.
+    This data type is mostly used internally, for hading over data in (de-)serialization operations.
     """
 
     @classmethod

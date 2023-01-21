e37776ac126fe056aad28d16f351705022ce9c25
Markus Binsteiner <markus@frkl.io>
2023-01-21 17:52:36 +0000

    chore: mypy fix

--- src/kiara/utils/graphs.py
+++ src/kiara/utils/graphs.py
@@ -69,7 +69,9 @@ def save_image(graph: nx.Graph, path: str):
         f.write(graph_b)
 
 
-def graph_to_image(graph: nx.Graph, return_bytes: bool = False) -> Union[bytes, Image]:
+def graph_to_image(
+    graph: nx.Graph, return_bytes: bool = False
+) -> Union[bytes, "Image"]:
 
     b = create_image(graph=graph)
 

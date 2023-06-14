2b14e14303204e53d1f7c53b629bac58c4ed98b9
Markus Binsteiner <markus@frkl.io>
2023-06-14 15:01:32 +0200

    chore: minor

--- src/kiara/interfaces/python_api/models/job.py
+++ src/kiara/interfaces/python_api/models/job.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, Any, Dict, List, Mapping, Union
 from pydantic import BaseModel, Field
 
 from kiara.exceptions import KiaraException
+from kiara.utils.cli import terminal_print
 from kiara.utils.files import get_data_from_file
 from kiara.utils.string_vars import replace_var_names_in_obj
 
@@ -82,56 +83,72 @@ class JobTest(object):
 
     def run_tests(self):
 
+        print(f"Running tests for job '{self._job_desc.job_alias}'...")  # noqa
+
         result = self.run_job()
         self.check_result(result)
 
     def run_job(self) -> "ValueMap":
 
-        result = self._kiara_api.run_job(operation=self._job_desc)
+        print(f"Running checks for job '{self._job_desc.job_alias}'...")  # noqa
+        try:
+            result = self._kiara_api.run_job(operation=self._job_desc)
+        except Exception as e:
+            exc = KiaraException(f"Failed to run job '{self._job_desc.job_alias}': {e}")
+            terminal_print(exc)
+            raise e
         return result
 
     def check_result(self, result: "ValueMap"):
 
-        import inspect
+        try:
 
-        from kiara.api import Value
+            import inspect
 
-        for test_name, test in self._tests.items():
+            from kiara.api import Value
 
-            if not callable(test):
-                tokens = test_name.split("::")
-                value = result.get_value_obj(tokens[0])
+            for test_name, test in self._tests.items():
 
-                if len(tokens) > 1:
-                    data_to_test = self._kiara_api.query_value(
-                        value, "::".join(tokens[1:])
-                    )
-                else:
-                    data_to_test = value
+                if not callable(test):
+                    tokens = test_name.split("::")
+                    value = result.get_value_obj(tokens[0])
 
-                if isinstance(data_to_test, Value):
-                    data_to_test = data_to_test.data
-
-                if test != data_to_test:
-                    raise AssertionError(
-                        f"Test pattern '{test_name}' for job '{self._job_desc.job_alias}' failed: {data_to_test} (result) != {test} (expected)"
-                    )
-
-            else:
-
-                args = inspect.signature(test)
-                arg_values: List[Any] = []
-
-                for arg_name in args.parameters.keys():
-                    if arg_name == "kiara_api":
-                        arg_values.append(self._kiara_api)
-                    elif arg_name == "outputs":
-                        arg_values.append(result)
-                    elif arg_name in result.field_names:
-                        arg_values.append(result.get_value_obj(arg_name))
+                    if len(tokens) > 1:
+                        data_to_test = self._kiara_api.query_value(
+                            value, "::".join(tokens[1:])
+                        )
                     else:
-                        raise KiaraException(
-                            f"Invalid test function: '{test_name}', argument '{arg_name}' not available in result. Available arguments: {result.field_names} or 'outputs' for all outputs."
+                        data_to_test = value
+
+                    if isinstance(data_to_test, Value):
+                        data_to_test = data_to_test.data
+
+                    if test != data_to_test:
+                        raise AssertionError(
+                            f"Test pattern '{test_name}' for job '{self._job_desc.job_alias}' failed: {data_to_test} (result) != {test} (expected)"
                         )
 
-                test(*arg_values)
+                else:
+
+                    args = inspect.signature(test)
+                    arg_values: List[Any] = []
+
+                    for arg_name in args.parameters.keys():
+                        if arg_name == "kiara_api":
+                            arg_values.append(self._kiara_api)
+                        elif arg_name == "outputs":
+                            arg_values.append(result)
+                        elif arg_name in result.field_names:
+                            arg_values.append(result.get_value_obj(arg_name))
+                        else:
+                            raise KiaraException(
+                                f"Invalid test function: '{test_name}', argument '{arg_name}' not available in result. Available arguments: {result.field_names} or 'outputs' for all outputs."
+                            )
+                    test(*arg_values)
+        except Exception as e:
+            exc = KiaraException(
+                f"Failed to run test '{test}' for job '{self._job_desc.job_alias}': {e}"
+            )
+            terminal_print(exc)
+            raise e
+        return result

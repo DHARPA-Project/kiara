8ed04bcd4e7a92aed10857f2f3b9de91449cd793
Markus Binsteiner <markus@frkl.io>
2024-04-08 15:32:30 +0200

    test: remove debug test

--- tests/test_archives/test_archive_import.py
+++ tests/test_archives/test_archive_import.py
@@ -11,40 +11,6 @@ TEST_RESOURCES_FOLDER = os.path.join(ROOT_DIR, "tests", "resources")
 VALUE_ID = "edbd6711-0432-430f-a147-08a6ae9df220"
 
 
-def test_debug_ci(api: BaseAPI):
-
-    resources_folder = Path(TEST_RESOURCES_FOLDER)
-    if not resources_folder.exists():
-        parent = resources_folder.parent.exists()
-        parent_parent = resources_folder.parent.parent.exists()
-        parent_parent_parent = resources_folder.parent.parent.parent.exists()
-        raise FileNotFoundError(
-            f"Resources folder not found: {resources_folder} - {parent} - {parent_parent} - {parent_parent_parent}"
-        )
-
-    if not resources_folder.is_dir():
-        raise NotADirectoryError(
-            f"Resources folder is not a directory: {resources_folder}"
-        )
-
-    def print_directory_contents(path, result_string):
-        for root, dirs, files in os.walk(path):
-            level = root.replace(path, "").count(os.sep)
-            indent = " " * 4 * level
-            result_string += f"\n{indent}{os.path.basename(root)}/"
-            subindent = " " * 4 * (level + 1)
-            for f in files:
-                result_string += f"\n{subindent}{f}"
-
-        return result_string
-
-    result_string = print_directory_contents(
-        resources_folder.as_posix(), f"{resources_folder.as_posix()}"
-    )
-
-    raise Exception(result_string)
-
-
 def test_archive_import_values_no_alias(api: BaseAPI):
 
     resources_folder = Path(TEST_RESOURCES_FOLDER)
